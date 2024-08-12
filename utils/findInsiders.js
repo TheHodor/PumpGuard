09f58fed32e82eabf1512317897df3d68295ac15
gindu dheer <defibuildersweb3@gmail.com>
2024-08-11 21:37:04 -0400

    add insider checks

---
+++ utils/findInsiders.js
@@ -0,0 +1,126 @@
+const fetch = require('node-fetch');
+
+const INSIDER_HARD_CAP = 100
+const RPC_helius = "https://mainnet.helius-rpc.com/?api-key=0923441f-c558-4e0b-8b67-9294161fdfb3"
+
+
+async function fetchSignatures(address) {
+    // Limit is 1000 max
+    const params = {
+        jsonrpc: '2.0',
+        id: 1,
+        method: 'getSignaturesForAddress',
+        params: [
+            address,
+            {
+                limit: INSIDER_HARD_CAP
+            }
+        ]
+    };
+
+    try {
+        const response = await fetch(RPC_helius, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        });
+
+        const data = await response.json();
+        const allTransactions = data.result
+        if (allTransactions.length === 0) {
+            console.log('No transactions found for this address.');
+            return null;
+        }
+        const lastTransaction = allTransactions[allTransactions.length - 1].signature
+        console.log('User Last signature: ', lastTransaction)
+        return lastTransaction
+
+    } catch (error) {
+        console.error(`Failed to fetch from helius for address ${address}:  ${error}`);
+        throw error;
+    }
+}
+
+
+async function parseAndProcessTransactions(trxn) {
+    const url = "https://api.helius.xyz/v0/transactions/?api-key=0923441f-c558-4e0b-8b67-9294161fdfb3";
+
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            transactions: [trxn],
+        }),
+    });
+
+    const data = await response.json();
+
+    const currentTime = new Date();
+
+    const parsedResults = data.map(transaction => {
+        if (transaction.type === 'TRANSFER') {
+            const fundingWallet = transaction.feePayer;
+            const date = new Date(transaction.timestamp * 1000);
+
+            const timeDifference = currentTime - date;
+            const hoursDifference = timeDifference / (1000 * 60 * 60);
+            const isFreshy = hoursDifference < 48;
+
+            console.log('Transaction Date:', date.toUTCString());
+            console.log('Funding wallet:', fundingWallet);
+            console.log('Was wallet funded within 48 hours?', isFreshy);
+
+            return {
+                date: date,
+                fundingWallet: fundingWallet,
+                isFreshy: isFreshy
+            };
+        }
+        return null;
+    });
+
+    const validResults = parsedResults.filter(result => result !== null);
+
+    return validResults;
+}
+
+const walletList = ['126vudNs2ezwk5EQ8XEXAd2U25crGQ86AWS1mcVYN9fU']
+// pass holders into this
+async function findSuspiciousWallets(walletList) {
+    let suspiousWallets = []
+    const allPromises = walletList.map(async (wallet) => {
+        const firstTrxn = await fetchSignatures(wallet)
+        // Most likely a fresh wallet or some insider shit
+        if (firstTrxn) {
+            console.log(`Wallet - ${wallet} is most likely insider.... Checking last trxn/funding status`)
+            const parsedData = await parseAndProcessTransactions(firstTrxn)
+            if (parsedData && parsedData.length > 0) {
+                parsedData.forEach(transaction => {
+                    if (transaction.isFreshy) {
+                        suspiousWallets.push(wallet);
+                    }
+                });
+            } else {
+                console.log('No valid transactions found or the transaction type is not TRANSFER.');
+            }
+        }
+
+    })
+    await Promise.all(allPromises);
+
+    // Log the list of suspicious wallets after all processing is done
+    console.log('List of insider wallets: ', suspiousWallets);
+
+}
+
+// findSuspiciousWallets(walletList)
+
+
+module.exports = {
+    findSuspiciousWallets
+}
