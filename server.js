dcdabbbaf1217f8ae2f7611ae51c21ee24c83a49
TheHodor <150680679+TheHodor@users.noreply.github.com>
2024-08-12 05:25:07 +0330

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const {
 } = require('./utils/DB_setup.js');
 const {
     getCoinLockAddress,
-    updateLockAddressBalance
+    updateLockAddressBalance,
+    isCoinGuarded
 } = require('./utils/guardCoins.js');
 
 
@@ -29,7 +30,7 @@ app.use(cors());
 // ----- setting express app ----- //
 
 
-let topCoinsOnPumpfun, _Collections, _DBs
+let allGuardedCoins_byPumpGuard, topProgressCoins, topGuardedCoins, recentlyGuardedCoins, _Collections, _DBs
 
 
 
@@ -46,20 +47,64 @@ async function serverStarted() {
     _DBs = _DBSetup.DBs
 
     // fetch top coins on pump.fun
-    setInterval(async () => {
-        topCoinsOnPumpfun = await PumpFunFetch.updateTopCoins()
-    }, 1000 * 5)
+    PrepareCoinsForFE()
 }
 
 
+// for listing coins on front end
+// the timeouts are set to avoid getting rate limited
+async function PrepareCoinsForFE() {
+    allGuardedCoins_byPumpGuard = await _Collections.GuardedCoins.find({}).toArray()
 
+    topProgressCoins = addLockedSolForCoins(await PumpFunFetch.getTopProgressCoins())
+
+    setTimeout(async () => {
+        topGuardedCoins = addLockedSolForCoins(await PumpFunFetch.getTopGuardedCoins())
+    }, 3000)
+
+    setTimeout(async () => {
+        recentlyGuardedCoins = addLockedSolForCoins(await PumpFunFetch.getRecentlyGuardedCoins())
+    }, 5000)
+
+    // adding locked sol to pump.fun coins for front-end display
+    function addLockedSolForCoins(pumpfunCoins) {
+        for (var i = 0; i < pumpfunCoins.length; i++) {
+            for (var j = 0; j < allGuardedCoins_byPumpGuard.length; j++) {
+
+                if (pumpfunCoins[i].mint == allGuardedCoins_byPumpGuard[j].ca) {
+                    pumpfunCoins[i].lockedSol = allGuardedCoins_byPumpGuard[j].balance
+                }
+            }
+        }
+
+        return pumpfunCoins
+    }
+
+    setTimeout(() => {
+        PrepareCoinsForFE()
+    }, 1000 * 30)
+}
 
 
 
 
 // front end request to get the top coins
 app.post('/get_top_coins', async (req, res) => {
-    res.json(topCoinsOnPumpfun);
+    res.json(topProgressCoins);
+});
+// front end request to get the top coins - Top Guarded Coins - and Recently guarded coins
+app.post('/get_all_coins', async (req, res) => {
+    res.json({
+        topCoins: topProgressCoins.slice(0, 25),
+        topGuarded: topGuardedCoins.slice(0, 25),
+        recentlyGuarded: recentlyGuardedCoins.slice(0, 25),
+    })
+});
+
+// user request to look up a coin and check if it's guarded or not + it's data for front end
+app.post('/is_coin_guarded', async (req, res) => {
+    const data = await isCoinGuarded(req.body.ca)
+    res.send(data)
 });
 
 // user request to get lock address for a coin
