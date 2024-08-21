f3dc9d6e3401dff573c281fdb7f5ebf0354ea4e0
gindu dheer <defibuildersweb3@gmail.com>
2024-08-21 18:46:31 -0400

    remove logs

--- server.js
+++ server.js
@@ -84,12 +84,10 @@ async function serverStarted() {
 async function PrepareCoinsForFE() {
     allGuardedCoins_byPumpGuard = await _Collections.GuardedCoins.find({}).toArray()
 
-    console.log('All guarded tokens: ', allGuardedCoins_byPumpGuard)
     let topProgress = await PumpFunFetch.getTopProgressCoins()
     if(!topProgress) {
         return null;
     }
-    console.log('All topProgress tokens: ', topProgress)
 
     topProgressCoins = addLockedSolForCoins(topProgress)
 
