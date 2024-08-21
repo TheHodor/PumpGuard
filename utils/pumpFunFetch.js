5b50c6838a86ad5ae41b4b64361145eef70dbb9e
TheHodor <150680679+TheHodor@users.noreply.github.com>
2024-08-22 02:18:29 +0330

    Update pumpFunFetch.js

--- utils/pumpFunFetch.js
+++ utils/pumpFunFetch.js
@@ -19,129 +19,132 @@ const VOLUME_UPDATERATE_MIN = 20
 
 
 async function getTopProgressCoins() {
-        let allTopCoins = []
+    let allTopCoins = []
+    // each time we fetch 50 tokens
+    // so... 5 = 250 tokens will be checked
+    const howManyTimes = 7
 
-        // each time we fetch 50 tokens
-        // so... 5 = 250 tokens will be checked
-        const howManyTimes = 7
+    for (let i = 0; i < howManyTimes; i++) {
+        let offset = i * 50
+        let _URL =
+            `https://frontend-api.pump.fun/coins?offset=${offset}&limit=50&sort=last_trade_timestamp&order=DESC&includeNsfw=true&complete=false`
 
-        for (let i = 0; i < howManyTimes; i++) {
-            let offset = i * 50
-            let _URL =
-                `https://frontend-api.pump.fun/coins?offset=${offset}&limit=50&sort=last_trade_timestamp&order=DESC&includeNsfw=true&complete=false`
-
-            try {
-                let response = await fetch(_URL)
-                let data = await response.json()
-                let sortedTokens = data.slice().sort((a, b) => b.usd_market_cap - a.usd_market_cap)
-                allTopCoins = [...allTopCoins, ...sortedTokens]
-            } catch (error) {
-                console.log("E.103", error)
-                return null
-            }
+        try {
+            let response = await fetch(_URL)
+            let data = await response.json()
+            let sortedTokens = data.slice().sort((a, b) => b.usd_market_cap - a.usd_market_cap)
+            allTopCoins = [...allTopCoins, ...sortedTokens]
+        } catch (error) {
+            console.log("E.103", error)
         }
+    }
 
-        // If you want to sort allTopCoins once more globally by usd_market_cap
-        allTopCoins.sort((a, b) => b.usd_market_cap - a.usd_market_cap)
+    // If you want to sort allTopCoins once more globally by usd_market_cap
+    allTopCoins.sort((a, b) => b.usd_market_cap - a.usd_market_cap)
 
-        allTopCoins = await setHolders(allTopCoins)
-        allTopCoins = await setVolume(allTopCoins)
-        return allTopCoins
+    allTopCoins = allTopCoins.slice(0, 20)
+    allTopCoins = await setHolders(allTopCoins)
+    allTopCoins = await setVolume(allTopCoins)
+
+console.log("allTopCoins.length", allTopCoins.length)
+    return allTopCoins
 }
 
 
 async function getTopGuardedCoins() {
-        let topGuarded = []
-        const _topGuarded = await _Collections.GuardedCoins.find({}).sort({
-            balance: -1
-        }).limit(20).toArray()
+    let topGuarded = []
+    const _topGuarded = await _Collections.GuardedCoins.find({}).sort({
+        balance: -1
+    }).limit(20).toArray()
 
-        for (const item of _topGuarded) {
-            if (!item.ca || item.ca.length < 30) continue
+    for (const item of _topGuarded) {
+        if (!item.ca || item.ca.length < 30) continue
 
-            try {
-                const response = await fetch(`https://frontend-api.pump.fun/coins/${item.ca}`);
-                const data = await response.json();
+        try {
+            const response = await fetch(`https://frontend-api.pump.fun/coins/${item.ca}`);
+            const data = await response.json();
 
-                if (data.creator && data.creator.length > 39 && data.creator.length < 49) {
-                    topGuarded.push(data);
-                }
-            } catch (error) {
-                console.log(`E.104 for ${item.ca}: `, error);
-                return null
+            if (data.creator && data.creator.length > 39 && data.creator.length < 49) {
+                topGuarded.push(data);
             }
-
-            // Add a 2-second delay after each fetch
-            await delay(2000);
+        } catch (error) {
+            console.log(`E.104 for ${item.ca}: `, error);
         }
 
-        topGuarded = await setHolders(topGuarded)
-        topGuarded = await setVolume(topGuarded)
-        return topGuarded.slice(0, 20)
+        // Add a 2-second delay after each fetch
+        await delay(2000);
+    }
 
+    topGuarded = topGuarded.slice(0, 20)
+    topGuarded = await setHolders(topGuarded)
+    topGuarded = await setVolume(topGuarded)
+
+    return topGuarded
 }
 
 
 async function getRecentlyGuardedCoins() {
+    let recentlyGuarded = []
+    const _recentlyGuarded = await _Collections.GuardedCoins.find({}).sort({
+        firstDeposit: 1
+    }).limit(20).toArray()
 
-        let recentlyGuarded = []
-        const _recentlyGuarded = await _Collections.GuardedCoins.find({}).sort({
-            firstDeposit: 1
-        }).limit(20).toArray()
+    for (const item of _recentlyGuarded) {
+        if (!item.ca || item.ca.length < 30) continue
 
-        for (const item of _recentlyGuarded) {
-            if (!item.ca || item.ca.length < 30) continue
+        try {
+            const response = await fetch(`https://frontend-api.pump.fun/coins/${item.ca}`);
+            const data = await response.json();
 
-            try {
-                const response = await fetch(`https://frontend-api.pump.fun/coins/${item.ca}`);
-                const data = await response.json();
-
-                if (data.creator && data.creator.length > 39 && data.creator.length < 49) {
-                    recentlyGuarded.push(data);
-                }
-            } catch (error) {
-                console.log(`E.104 for ${item.ca}: `, error);
-                return null
+            if (data.creator && data.creator.length > 39 && data.creator.length < 49) {
+                recentlyGuarded.push(data);
             }
-
-            // Add a 2-second delay after each fetch
-            await delay(2000);
+        } catch (error) {
+            console.log(`E.104 for ${item.ca}: `, error);
         }
 
-        recentlyGuarded = await setHolders(recentlyGuarded)
-        recentlyGuarded = await setVolume(recentlyGuarded)
-        return recentlyGuarded.slice(0, 20)
+        // Add a 2-second delay after each fetch
+        await delay(2000);
+    }
+
+    recentlyGuarded = recentlyGuarded.slice(0, 20)
+    recentlyGuarded = await setHolders(recentlyGuarded)
+    recentlyGuarded = await setVolume(recentlyGuarded)
+
+    return recentlyGuarded
 }
 
 
 async function setHolders(coins) {
-    for (var i = 0; i < coins.length; i++) {
+    for (const coin of coins) {
         // find coin holders
-        const __ca = coins[i].mint
-        const keyExists = Object.keys(holdersCount).includes(__ca)
-        let _tokenHolders
+        const __ca = coin.mint;
+        const keyExists = Object.keys(holdersCount).includes(__ca);
+        let _tokenHolders;
 
         // if holders count already exists and it's not too old
         if (keyExists && holdersCount[__ca].lastFetched < Date.now() - 1000 * HOLDERS_UPDATERATE_MIN) {
-            _tokenHolders = holdersCount[__ca].count
+            _tokenHolders = holdersCount[__ca].count;
         } else {
-            _tokenHolders = await getCoinHolders(__ca)
-            if (!_tokenHolders) continue
+            _tokenHolders = await getCoinHolders(__ca);
+            if (!_tokenHolders) continue;
 
             holdersCount[__ca] = {
                 count: _tokenHolders,
-                lastFetched: Date.now()
-            }
+                lastFetched: Date.now(),
+            };
         }
 
-        coins[i].holders = _tokenHolders
+        coin.holders = _tokenHolders;
     }
 
     return coins
 }
+
+
 let _____calc = false
 async function setVolume(coins) {
-    return 0
+    return coins
     for (var i = 0; i < coins.length; i++) {
         if (_____calc) block
         // find coin holders
@@ -183,22 +186,22 @@ async function getAllTradesPump(ca) {
         while (true) {
             const response = await fetch(
                 `https://frontend-api.pump.fun/trades/${ca}?limit=200&offset=${offset}`, {
-                headers: {
-                    accept: '*/*',
-                    'accept-language': 'en-US,en;q=0.8',
-                    'if-none-match': 'W/"12266-/HJ/xj010RRztcqVlXCQtyB5KTs"',
-                    'sec-ch-ua': '"Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'empty',
-                    'sec-fetch-mode': 'cors',
-                    'sec-fetch-site': 'cross-site',
-                    'sec-gpc': '1',
-                    Referer: 'https://www.pump.fun/',
-                    'Referrer-Policy': 'strict-origin-when-cross-origin',
-                },
-                method: 'GET',
-            }
+                    headers: {
+                        accept: '*/*',
+                        'accept-language': 'en-US,en;q=0.8',
+                        'if-none-match': 'W/"12266-/HJ/xj010RRztcqVlXCQtyB5KTs"',
+                        'sec-ch-ua': '"Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+                        'sec-ch-ua-mobile': '?0',
+                        'sec-ch-ua-platform': '"Windows"',
+                        'sec-fetch-dest': 'empty',
+                        'sec-fetch-mode': 'cors',
+                        'sec-fetch-site': 'cross-site',
+                        'sec-gpc': '1',
+                        Referer: 'https://www.pump.fun/',
+                        'Referrer-Policy': 'strict-origin-when-cross-origin',
+                    },
+                    method: 'GET',
+                }
             );
             if (response) {
                 const result = await response.json();
